9aa075ded4acfdfabac75b97e49f98f9a324c86a
okoye <okoye9@hotmail.com>
2010-03-04 21:42:41 -0600

    fixed certain slice_predicate bug

--- tests/hello.py
+++ tests/hello.py
@@ -21,28 +21,33 @@ def main():
    keyspace = "Twitter"
    column_path = ColumnPath(column_family="Timeline",column="message")
    key = "pinggoat"
-   value = "hello world!, my first cassandra input"
+   value = " hello world!, my first cassandra input "
    timestamp = time.time()
    consistency = ConsistencyLevel.ONE
    column_parent = ColumnParent(column_family="Timeline")
    column_names = list("message")
-   slice_predicate =\
-   SlicePredicate(column_names,SliceRange(start='',finish='',reversed=False,count=100))
+   slice_predicate =SlicePredicate(column_names)
    
    #Insert data into cassandra database with already defined schema
 
    try:
+      transport.open()
       client.insert(keyspace,key,column_path,value,timestamp,consistency)
    except Thrift.TException, tex:
       print 'Thrift: %s' % tex.message
+   finally:
+      transport.close()
 
    #Now retrieve data from cassandra database
    try:
+      transport.open()
       data = client.get_slice(keyspace,key,column_parent,\
                               slice_predicate,consistency)
       print "hello"
    except Thrift.TException, tex:
       print 'Thrift %s' % tex.message
+   finally:
+      transport.close()
 
 if __name__ == '__main__':
    print "Running cassandra program\n"
