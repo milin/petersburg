8859f9cd0bfd53695c260f06d2982f0af9023f95
okoye <okoye9@hotmail.com>
2010-03-04 21:18:43 -0600

    insert and retrieve data

--- tests/hello.py
+++ tests/hello.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-#
+#@author: Chuka Okoye
 #Copyright info here
 #
 #
@@ -21,10 +21,26 @@ def main():
    value = "hello world!, my first cassandra input"
    timestamp = time.time()
    consistency = ConsistencyLevel.ONE
+   column_parent = ColumnParent(column_family="Timeline")
+   column_names = list("message")
+   slice_predicate =\
+   SlicePredicate(column_names,SliceRange(start='',finish='',reversed=False,count=100))
+   
+   #Insert data into cassandra database with already defined schema
+
    try:
       client.insert(keyspace,key,column_path,value,timestamp,consistency)
    except Thrift.TException, tex:
       print 'Thrift: %s' % tex.message
 
-if __name__ == ' __main__':
+   #Now retrieve data from cassandra database
+   try:
+      data = client.get_slice(keyspace,key,column_parent,\
+                              slice_predicate,consistency)
+      print "hello"
+   except Thrift.TException, tex:
+      print 'Thrift %s' % tex.message
+
+if __name__ == '__main__':
+   print "Running cassandra program\n"
    main()
