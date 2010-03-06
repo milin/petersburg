c53b07ebe4148f542057e6b9388b911012cd46b2
okoye <okoye9@hotmail.com>
2010-03-05 20:24:55 -0600

    completed authentication tests

---
+++ tests/test_authenticate.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+#
+#Copyright info here
+#
+#
+
+import sys
+sys.path.append("../")
+import unittest
+import getpass
+import Ingest
+
+class TestAuthentication(unittest.TestCase):
+   def setUp(self):
+      self.username = raw_input("enter username: ")
+      self.password = getpass.getpass("enter password: ")
+   
+   def test_authenticate(self):
+      x = Ingest.Twitter()
+      portal = x.authenticate(self.username, self.password)
+      portal.update_status("hello world test from cscbot1")
+
+if __name__ == '__main__':
+   unittest.main()
